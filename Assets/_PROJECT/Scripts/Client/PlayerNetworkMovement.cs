eeb9d60916b5e60c3ec92b018d0b7feaf8bd93b9
Simon du Toit <simon@gssr.co.za>
2025-10-03 12:59:08 +0200

    Tested networkvariables

--- Assets/_PROJECT/Scripts/Client/PlayerNetworkMovement.cs
+++ Assets/_PROJECT/Scripts/Client/PlayerNetworkMovement.cs
@@ -4,14 +4,29 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class PlayerNetworkMovement : NetworkBehaviour
 {
     private CharacterInput _inputMap;
     
+    [Header("Player Data")]
+    private NetworkVariable<float> _playerHealth = new NetworkVariable<float>(100, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    private NetworkVariable<CustomPlayerData> _playerData = new NetworkVariable<CustomPlayerData>(new CustomPlayerData
+    {
+        playerID = 5,
+        isdead = false
+    });
+
+    private struct CustomPlayerData
+    {
+      public int playerID;
+      public bool isdead;
+    }
+    
     [Header("Movement")]
     private Rigidbody _characterRb;
-    private Transform _playerTransform;
     private Vector3 _movementVector;
     [SerializeField] private float moveSpeedMultiplier = 5f, dashSpeedMultiplier = 1.5f;
     private float _actualSpeed;
@@ -20,6 +35,7 @@ public class PlayerNetworkMovement : NetworkBehaviour
     [SerializeField] private Transform groundCheckTransform;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundCheckRadius = 0.5f, jumpMultiplier = 5f;
+    
     private void Awake()
     {
         _inputMap = new CharacterInput();
@@ -33,11 +49,6 @@ public class PlayerNetworkMovement : NetworkBehaviour
         if (_characterRb == null)
         {
             _characterRb = GetComponent<Rigidbody>();
-        }
-
-        if (_playerTransform == null)
-        {
-            _playerTransform = GetComponent<Transform>();
         }
     }
 
@@ -55,7 +66,11 @@ public class PlayerNetworkMovement : NetworkBehaviour
     {
         _actualSpeed = moveSpeedMultiplier;
     }
-    
+
+    public override void OnNetworkSpawn()
+    {
+        _playerHealth.Value = Random.Range(0, 100);
+    }
     private void FixedUpdate()
     {
         if (!IsOwner) return;
@@ -64,20 +79,24 @@ public class PlayerNetworkMovement : NetworkBehaviour
 
     private void OnMove(InputAction.CallbackContext context)
     {
+        if (!IsOwner) return;
         _movementVector = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
     }
 
     private void ResetMovement(InputAction.CallbackContext context)
     {
+        if (!IsOwner) return;
         _movementVector = Vector3.zero;
         _actualSpeed = moveSpeedMultiplier;
     }
     private void OnDash(InputAction.CallbackContext context)
     {
+        if (!IsOwner) return;
         _actualSpeed = moveSpeedMultiplier * dashSpeedMultiplier;
     }
     private void OnJump(InputAction.CallbackContext context)
     {
+        if (!IsOwner) return;
         var groundArray = Physics.OverlapSphere(groundCheckTransform.position, groundCheckRadius, groundLayer);
         if (groundArray.Length == 0) return;
         var jumpVector = new Vector3(0, jumpMultiplier, 0);
@@ -85,7 +104,8 @@ public class PlayerNetworkMovement : NetworkBehaviour
     }
     private void OnPause(InputAction.CallbackContext context)
     {
-        //Pause Game
+        if (!IsOwner) return;
+        Debug.Log($"{OwnerClientId}'s health is at {_playerHealth.Value}");
     }
     
     private void OnDrawGizmos()
